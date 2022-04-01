bd36786f87ee95edde45a95a6c647de574c8f1a0
tbuzmakov <tbuzmakov@yoobic.com>
2022-04-01 16:53:53 -0700

    feat(fron-page): Implement Posts Filter

--- design-system/src/components/recent-activity/recent-activity.tsx
+++ design-system/src/components/recent-activity/recent-activity.tsx
@@ -1,6 +1,10 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, h, State } from '@stencil/core';
 import { cards } from '../../global/data/activity-cards.data';
  
+type filterType = 'all' | 'addedPhoto' | 'wroteReview';
+let all = 'selected';
+let photoAdded = '';
+let wroteReview = '';
 @Component({
   tag: 'recent-activity',
   styleUrl: 'recent-activity.scss',
@@ -8,40 +12,71 @@ import { cards } from '../../global/data/activity-cards.data';
 })
 export class RecentActivity {
 
+  @State() filter: filterType = 'all';
 
-  renderCards() {
-      return(
-        <span>
-            {
-              cards.map((card) => (
-                
-                <activity-card post={card}></activity-card>   
-                
-              ))
-            }
-        </span>
-      );
+
+  setFilter(filterTo: filterType){
+    this.filter = filterTo;
+
+    switch(filterTo){
+      case 'all':
+        all = 'selected';
+        photoAdded = '';
+        wroteReview = '';
+        break;
+      case 'addedPhoto':
+        all = '';
+        photoAdded = 'selected';
+        wroteReview = '';
+        break;
+      case 'wroteReview':
+        all = '';
+        photoAdded = '';
+        wroteReview = 'selected';
+        break;
+    }
   }
 
-  render() {
+  renderCards() {
+    let cardsToRender;
+
+    if(this.filter === 'all'){
+      cardsToRender = cards.map((post) => post)
+    } else {
+      cardsToRender = cards.filter((post) => post.activity === this.filter)
+    }
+      return(
+        <div class="recent-activity-container">
+              {cardsToRender.map((card) => (
+                  <activity-card post={card}></activity-card>   
+                ))
+              }
+        </div>
+      )
+    }
+
+  renderFilterSelector(){
+      return(
+        <div class="filter-selector">
+          <div class={all} onClick={() => this.setFilter('all')}>All</div>
+          <span>|</span>
+          <div class={photoAdded} onClick={() => this.setFilter('addedPhoto')}>Photo added</div>
+          <span>|</span>
+          <div class={wroteReview} onClick={() => this.setFilter('wroteReview')}>Review</div>
+        </div>
+      )
+  }
   
+
+  render() {
     return (
       <Host>
         <div class="recent-activity-wrapper">
-          <h3>Recent Activity</h3>
-          <div class="recent-activity-container">
-  
-              {/* {this.renderCards()}
-              {this.renderCards()} */}
-              {cards.map((card) => (
-                
-                <activity-card post={card}></activity-card>   
-                
-              ))
-              }
-          
-          </div>
-          
+          <h2>Recent Activity</h2>
+              
+              {this.renderFilterSelector()}
+              {this.renderCards()}
+
         </div>
         
       </Host>
