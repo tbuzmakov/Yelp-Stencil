7fe14adf0218963043ffded0f41c6784ebf47beb
tbuzmakov <tbuzmakov@yoobic.com>
2022-04-05 16:06:14 -0700

    refactor: start component and style

--- design-system/src/components/star-component/star-component.tsx
+++ design-system/src/components/star-component/star-component.tsx
@@ -12,50 +12,14 @@ export class StarComponent {
   
   @Prop() isSelected: boolean;
   @Prop() starSelected: number;
-  @State() starColor = '';
-  
-  componentWillLoad(){
-    this.setStarColor();
-  }
 
-  @Watch('starSelected')
-  watchPropHandler(newValue: number, oldValue: number) {
-    this.setStarColor();
-    }
 
-  setStarColor(){
-    switch(this.starSelected){
-      case 5:
-        this.starColor = 'five';
-        break;
-      case 4:
-        this.starColor = 'four';
-        break;
-      case 3:
-        this.starColor = 'three';
-        break;
-      case 2:
-        this.starColor = 'two';
-        break;
-      case 1:
-        this.starColor = 'one';
-        break;
-      }
-  }
-  
-
-  render() {
+  render() {    
     return (
       <Host>
-      {
-      this.isSelected ? 
-        <div class={`star-selected ${this.starColor}`}>
-          <img src={Star} alt="" />
-        </div>:  
-        <div class="star-not-selected">
+        <div class={{"star-selected": true, "star-not-selected": !this.isSelected}}>
           <img src={Star} alt="" />
         </div>
-      }
       </Host>
     );
   }
